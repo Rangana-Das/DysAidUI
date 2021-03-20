e9676c6eb17db140e2c23e3adf85636289296d4e
Rangana-Das <35832416+Rangana-Das@users.noreply.github.com>
2021-03-20 22:03:40 +0530

    Add files via upload

---
+++ dys_UI/main/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render,redirect
+from .forms import UserRegistrationForm
+
+def home(request):
+    return render(request, 'home.html')
+    
+def login(request):
+    return render(request,'login.html')
+    
+def reg(request):
+    if request.method == 'POST':
+        form=UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('main-login')
+    else:
+        form = UserRegistrationForm()
+    return render(request,'reg.html',{'form':form})
+
